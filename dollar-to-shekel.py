9dc31521cf32bd14977d8e47d81e1ba9b7d2110f
BlackShit <103965764+BrightShit@users.noreply.github.com>
2022-06-15 19:36:14 +0300

    Update dollar-to-shekel.py

--- dollar-to-shekel.py
+++ dollar-to-shekel.py
@@ -6,7 +6,7 @@ def dollar__():
     soup = BeautifulSoup(source,'lxml')
     dollar = soup.find("p",class_="result__BigRate-sc-1bsijpp-1 iGrAod")
     dollar__to__float=dollar.text.split(" ")
-    (Line 3-9) it will help me to get the updated dollar rate 
+    #(Line 3-9) it will help me to get the updated dollar rate 
     new__dollar=dollar__to__float[0]
     new__dollar = str(new__dollar)
     new__dollar = float(new__dollar)
@@ -21,4 +21,3 @@ def dollar__():
     print("This is:",sum__,"Israeli New Shekels")
     #(Line 14-22) Converting dollar to shekel
 print(dollar__())
-
