e1d916554e8df583f5d84f3c893fabf129828347
BlackShit <103965764+BrightShit@users.noreply.github.com>
2022-06-15 22:48:27 +0300

    Update dollar-to-shekel.py

--- dollar-to-shekel.py
+++ dollar-to-shekel.py
@@ -20,4 +20,31 @@ def dollar__():
     sum__=round(sum__,2)
     print("This is:",sum__,"Israeli New Shekels")
     #(Line 14-22) Converting dollar to shekel
-print(dollar__())
+
+#Convert shekel to dollar
+def shekel__():
+    google ='https://www.xe.com/currencyconverter/convert/?Amount=1&From=ILS&To=USD'
+    source = requests.get(google).text
+    soup = BeautifulSoup(source,'lxml')
+    dollar = soup.find("p",class_="result__BigRate-sc-1bsijpp-1 iGrAod")
+    dollar__to__float=dollar.text.split(" ")
+    #(Line 3-9) it will help me to get the updated dollar rate 
+    new__dollar=dollar__to__float[0]
+    new__dollar = str(new__dollar)
+    new__dollar = float(new__dollar)
+    #(Line 10-13)Converting the text into a float 
+    how__many__dollars=float(input("What is the price: \n"))
+    try:
+        float(how__many__dollars)
+    except:
+        print("Please choose a number")
+    sum__=float(new__dollar)*float(how__many__dollars)  
+    sum__=round(sum__,2)
+    print("This is:",sum__,"Israeli New Shekels")
+
+inta= int(input("Enter an option:\n 1) Dollar to shekel\n 2) Shekel to dollar\n"))
+if inta == 1:
+    print(dollar__())
+
+elif inta == 2:
+    print(shekel__())
